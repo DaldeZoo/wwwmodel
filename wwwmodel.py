e5407d381a1a3187b52749dae5ea5d9f91718006
DaldeZoo <sdaboys25@gmail.com>
2024-12-28 16:39:04 -0500

    fixed recursion bug, improved efficiency and readability - decision tree still needs fixing

--- wwwmodel.py
+++ wwwmodel.py
@@ -25,7 +25,7 @@ import numpy as np
 #   All in [1-10] scale NATURAL NUMBERS
 # TODO: improve? normalize? reduce dimensionality? hmm...
 
-NUM_OF_FEATURES = 8
+FEATURE_COUNT = 8
 MAX_THRESHOLD = 10
 MIN_THRESHOLD = -(MAX_THRESHOLD)
 MIN_NODE_DP_COUNT = 3
@@ -33,10 +33,11 @@ MIN_NODE_DP_COUNT = 3
 # The input to the model would be the vector of the difference between the two chosen character vectors
 # Then, this is inputted into the tree which classifies {1, 2}, where 1 means character 1 wins and 2 2 wins.
 
-NAME = 0
-STATS = 1
 # Example character tuples:
 # TODO: make a file for the data... and maybe get data via data scraping... where will i find this data lol
+# characters are represented by a tuple: idx 0 is their name and 1 is their stats/features vector
+NAME = 0
+STATS = 1
 GOJO = ("Gojo", np.array([10, 7, 10, 10, 7, 7, 9, 9]))
 GOKU = ("Goku", np.array([9, 10, 10, 8, 7, 6, 10, 10]))
 LUFFY = ("Luffy", np.array([8, 9, 0, 7, 7, 6, 8, 8]))
@@ -55,7 +56,9 @@ JOGO = ("Jogo", np.array([6, 7, 9, 6, 6, 7, 7, 7]))
 def get_input(character_one, character_two):
     return character_one[STATS] - character_two[STATS]
 
+
 # Temporary training datapoints:
+# these are labeled training datapoints ie (input, output)
 TRAINING_DATAPOINTS = [
     (get_input(GOJO, GOKU), 2),
     (get_input(GOKU, SAITAMA), 2),
@@ -78,14 +81,13 @@ TRAINING_DATAPOINTS = [
     (get_input(REINER, SHIKAMARU), 1),
     (get_input(LUFFY, GOKU), 2)
 ]
-OUTPUTS = [1, 2] # class 1 - character 1 wins, class 2 - character 2 wins
-
+OUTPUTS = (1, 2) # class 1 - character 1 wins, class 2 - character 2 wins
 class Node:
    def __init__(self, dp_list):
       self.dp_list = dp_list # list of all datapoints in node j
       self.dp_count = len(dp_list)
-      self.split_feature = None
-      self.split_threshold = None
+      self.split_feature = -1
+      self.split_threshold = -1
       self.left = None
       self.right = None
 
@@ -93,27 +95,26 @@ def entropy(dp_list):
     if len(dp_list) == 0:
         return 0
 
-    # each row is all the datapoints from node with class/output i
-    dp_classes_matrix = []
+    # each row i is the number of datapoints in dp_list with class i+1
+    dp_classes_count = []
     for i in OUTPUTS:
-        dp_classes_matrix.append([])
-    
+        dp_classes_count.append(0)
     for dp in dp_list:
         dp_output = dp[1]
-        dp_classes_matrix[dp_output-1].append(dp)
-    
+        dp_classes_count[dp_output-1] += 1
+
     sum = 0
-    for i in OUTPUTS:
-        if len(dp_classes_matrix[i-1]) != 0:
-            prob_i = len(dp_classes_matrix[i-1]) / len(dp_list) # probability of class i in node
-            sum += prob_i * log(prob_i, 2)
+    for i in range(len(OUTPUTS)):
+        if dp_classes_count[i] > 0:
+            class_probability = dp_classes_count[i] / len(dp_list) # probability of class i in node
+            sum += class_probability * log(class_probability, 2)
 
     return -(sum)
 
 # Using split_feature x > split_threshold z
 def information_gain(node, split_feature, split_threshold):
     if node is None or node.dp_count == 0:
-        return 0
+        return -1
 
     # datapoints that go to the left subtree or right subtrees, respectively, after split
     left_subtree_dps = []
@@ -131,9 +132,12 @@ def information_gain(node, split_feature, split_threshold):
     result += -(len(right_subtree_dps)/node.dp_count)*right_entropy
     return result
 
-# for root assumes: node = Node(TRAINING_DATAPOINTS)
+# removes datapoint list and count from each node, as is no longer needed after tree is built
+def clean_tree(root):
+    pass
+
 def build_decision_tree(node):
-    # base case
+    # base case: no uncertainty or num of dps small enough, then becomes leaf node
     if (entropy(node.dp_list) == 0) or (len(node.dp_list) < MIN_NODE_DP_COUNT):
         node.left = None
         node.right = None
@@ -141,8 +145,8 @@ def build_decision_tree(node):
 
     # getting best split function based on highest ig
     split_fcn = [-1, -1, -1]
-    for feature in range(NUM_OF_FEATURES):
-        for threshold in (MIN_THRESHOLD, MAX_THRESHOLD):
+    for feature in range(FEATURE_COUNT):
+        for threshold in range(MIN_THRESHOLD, MAX_THRESHOLD): # !infinite recursion fix was here lol!
             ig = information_gain(node, feature, threshold)
             if ig > split_fcn[0]:
                 split_fcn = [ig, feature, threshold]
@@ -169,7 +173,10 @@ def build_decision_tree(node):
     left_child_node = Node(left_dp_list)
     node.right = build_decision_tree(right_child_node)
     node.left = build_decision_tree(left_child_node)
+
+    clean_tree(node)
     return node
+# TODO: after tree is built, dp_list and counts in each node are pointless...
 
 def classify_node(node):
     class_one_count = 0
@@ -188,6 +195,7 @@ def classify_node(node):
         return '2'
 
 # assumes fully built decision tree
+# returns winner character tuple
 def get_winner(node, characters):
     split_feature = node.split_feature
     split_threshold = node.split_threshold
@@ -198,14 +206,17 @@ def get_winner(node, characters):
         else:
             node = node.left
     winner_label = classify_node(node)
-    return winner_label
-
+    if winner_label == '1':
+        return characters[0]
+    else:
+        return characters[1]
 
 # running and testing model
 node = Node(TRAINING_DATAPOINTS)
 root = build_decision_tree(node)
-fighters = (GOJO, GOKU)
-winner = get_winner(root, fighters)
-print(f"{fighters[winner]} wins!")
+characters = (GOJO, GOKU)
+winner = get_winner(root, characters)
+print(f"{winner[0]} wins!")
+# uhh... why did gojo win?
 
 # TODO: debug dis whole thing lol
